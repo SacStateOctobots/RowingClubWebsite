1de20cd4127d31e45184c0be554073ccc81d8daf
sangnguyen89 <sangnguyen2@csus.edu>
2023-09-17 10:50:03 -0700

    Edit expired day for selfsign key

--- Website/gen_https_key.sh
+++ Website/gen_https_key.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-openssl req -x509 -nodes -days 3650 -newkey ec:<(openssl ecparam -name prime256v1) -keyout private_key.pem -out certificate.pem
+openssl req -x509 -nodes -days 365 -newkey ec:<(openssl ecparam -name prime256v1) -keyout private_key.pem -out certificate.pem
