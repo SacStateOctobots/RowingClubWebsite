1ef14a9215ace78d1819a2bfec1829cf8feb7957
knightsamson <118357742+knightsamson@users.noreply.github.com>
2023-12-01 16:28:29 -0800

    Update welcome_page.py

--- Website/selenium_tests/welcome_page.py
+++ Website/selenium_tests/welcome_page.py
@@ -46,13 +46,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# import webdriver
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import datetime 
-from test_harness import run_tests, PATH_STRING
-
-# welcome page has no custom testing functionality so we just pass it an empty function
-if __name__ == "__main__":
-	run_tests(lambda x: None, PATH_STRING)
